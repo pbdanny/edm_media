9abb38f79a73e7b95eea06e4169704fbe64e4661
Danny Thanakrit <pb.danny@gmail.com>
2022-07-27 14:51:30 +0700

    Activated

--- instore_eval.py
+++ instore_eval.py
@@ -130,14 +130,14 @@ def get_cust_activated(
     cmp_brand_activated = _get_activated(exposed_cust=cmp_exposed, shppr_cust=cmp_brand_shppr)
 
     nmbr_brand_activated = cmp_brand_activated.count()
-    print(f'Total exposed and Feature Brand (in Category scope) shopper (Brand Activated) : {nmbr_brand_activated}')
+    print(f'Total exposed and Feature Brand (in Category scope) shopper (Brand Activated) : {nmbr_brand_activated:,d}')
     
     # Sku Activated
     cmp_sku_shppr = _get_shpper(txn=txn, period_wk_col_nm=period_wk_col, prd_scope_df=feat_sf)
     cmp_sku_activated = _get_activated(exposed_cust=cmp_exposed, shppr_cust=cmp_sku_shppr)
 
     nmbr_sku_activated = cmp_sku_activated.count()
-    print(f'Total exposed and Features SKU shopper (Features SKU Activated) : {nmbr_sku_activated}')
+    print(f'Total exposed and Features SKU shopper (Features SKU Activated) : {nmbr_sku_activated:,d}')
     
     return cmp_brand_activated, cmp_sku_activated
 
