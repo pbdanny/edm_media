7fbfa92763350ddd9174ea64373a393ab65aa840
pbdanny <pbdanny>
2023-02-22 11:20:27 +0000

    update outlier

--- instore_eval.py
+++ instore_eval.py
@@ -1830,13 +1830,15 @@ def get_store_matching_across_region(
     print(f"Outlier score threshold : {OUTLIER_SCORE_THRESHOLD}")
     print("Details of bad match pair(s)")
     outlier = flag_outlier[flag_outlier["flag_outlier"]]
-    if outlier.count() > 0:
+    
+    #if outlier.count() > 0:
+    if outlier.empty :  ## Pat change to use ".empty" to check empty df -- 14 feb 2023
+        print("No outlier (no bad match)")        
+    else: ## have bad match
         (outlier
         .merge(test_str_region, on="test_store_id", how="left")
         .merge(ctrl_str_region, on="ctrl_store_id", how="left")
         ).display()
-    else:
-        print("No outlier (no bad match)")
 
     # print("Pair plot bad match store")
     #__plt_pair(outlier, store_comp_score=store_comp_score)
@@ -3224,7 +3226,10 @@ def get_cust_activated_prmzn(
     # Brand activate
     target_str = _create_test_store_sf(test_store_sf=test_store_sf, cp_start_date=cp_start_date, cp_end_date=cp_end_date)
     cmp_exposed = _get_exposed_cust(txn=txn, test_store_sf=target_str, prd_scope_df=brand_sf)
-    cmp_brand_shppr = _get_shppr_prmzn(txn=txn, test_store_sf=target_str, period_wk_col_nm=period_wk_col, prd_scope_df=brand_sf)
+    #cmp_brand_shppr = _get_shppr_prmzn(txn=txn, test_store_sf=target_str, period_wk_col_nm=period_wk_col, prd_scope_df=brand_sf)
+    
+    ## Change column order to align with defined in function above -- Pat 22 Feb 2023
+    cmp_brand_shppr = _get_shppr_prmzn(txn=txn, period_wk_col_nm=period_wk_col, test_store_sf=target_str, prd_scope_df=brand_sf)
     cmp_brand_activated = _get_activated_prmzn(exposed_cust=cmp_exposed, shppr_cust=cmp_brand_shppr)
 
     #nmbr_brand_activated = cmp_brand_activated.count()
