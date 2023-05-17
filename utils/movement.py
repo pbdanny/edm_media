6db52d585c3253120954cc0eb01f7d759612e786
pbdanny <pb.danny@gmail.com>
2023-05-17 20:13:15 +0700

    add movement and switcing

---
+++ utils/movement.py
@@ -0,0 +1,220 @@
+import pprint
+from ast import literal_eval
+from typing import List
+from datetime import datetime, timedelta
+import sys
+import os
+
+import numpy as np
+
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql import DataFrame as SparkDataFrame
+from pyspark.sql import Window
+
+from .DBPath import DBPath
+from .campaign_config import CampaignEval
+from .exposure import create_txn_x_store_mech
+
+def get_cust_movement(cmp: CampaignEval,
+                      sku_activated: SparkDataFrame):
+    """Customer movement based on tagged feature activated & brand activated
+
+    """
+    cmp.spark.sparkContext.setCheckpointDir('dbfs:/FileStore/thanakrit/temp/checkpoint')
+    txn = cmp.txn
+    wk_type = cmp.wk_type
+    feat_sf = cmp.feat_sku
+    class_df = cmp.feat_class_sku
+    sclass_df = cmp.feat_subclass_sku
+    brand_df = cmp.feat_brand_sku
+    switching_lv = cmp.params["cate_lvl"]
+      
+    #---- Helper function
+    def _get_period_wk_col_nm(wk_type: str
+                              ) -> str:
+        """Column name for period week identification
+        """
+        if wk_type in ["promo_week", "promo_wk"]:
+            period_wk_col_nm = "period_promo_wk"
+        elif wk_type in ["promozone"]:
+            period_wk_col_nm = "period_promo_mv_wk"
+        else:
+            period_wk_col_nm = "period_fis_wk"
+        return period_wk_col_nm
+
+    #---- Main
+    # Movement
+    # Existing and New SKU buyer (movement at micro level)
+    print("-"*80)
+    print("Customer movement")
+    print("Movement consider only Feature SKU activated")
+    print("-"*80)
+
+    print("-"*80)
+    period_wk_col = _get_period_wk_col_nm(wk_type=wk_type)
+    print(f"Period PPP / PRE / CMP based on column {period_wk_col}")
+    print("-"*80)
+
+    # Features SKU movement
+    prior_pre_sku_shopper = \
+    (txn
+     .where(F.col('household_id').isNotNull())
+     .where(F.col(period_wk_col).isin(['pre', 'ppp']))
+     .join(feat_sf, "upc_id", "inner")
+     .select('household_id')
+     .drop_duplicates()
+    )
+
+    existing_exposed_cust_and_sku_shopper = \
+    (sku_activated
+     .select("household_id")
+     .join(prior_pre_sku_shopper, 'household_id', 'inner')
+     .withColumn('customer_macro_flag', F.lit('existing'))
+     .withColumn('customer_micro_flag', F.lit('existing_sku'))
+     .checkpoint()
+    )
+
+    new_exposed_cust_and_sku_shopper = \
+    (sku_activated
+     .select("household_id")
+     .join(existing_exposed_cust_and_sku_shopper, 'household_id', 'leftanti')
+     .withColumn('customer_macro_flag', F.lit('new'))
+     .checkpoint()
+    )
+
+    # Customer movement for Feature SKU
+    ## Macro level (New/Existing/Lapse)
+    prior_pre_cc_txn = \
+    (txn
+     .where(F.col('household_id').isNotNull())
+     .where(F.col(period_wk_col).isin(['pre', 'ppp']))
+    )
+
+    prior_pre_store_shopper = prior_pre_cc_txn.select('household_id').drop_duplicates()
+
+    prior_pre_class_shopper = \
+    (prior_pre_cc_txn
+     .join(class_df, "upc_id", "inner")
+     .select('household_id')
+     .drop_duplicates()
+    )
+
+    prior_pre_subclass_shopper = \
+    (prior_pre_cc_txn
+     .join(sclass_df, "upc_id", "inner")
+     .select('household_id')
+     .drop_duplicates()
+    )
+
+    ## Micro level
+    new_sku_new_store = \
+    (new_exposed_cust_and_sku_shopper
+     .join(prior_pre_store_shopper, 'household_id', 'leftanti')
+     .select('household_id', 'customer_macro_flag')
+     .withColumn('customer_micro_flag', F.lit('new_to_lotus'))
+    )
+
+    new_sku_new_class = \
+    (new_exposed_cust_and_sku_shopper
+     .join(prior_pre_store_shopper, 'household_id', 'inner')
+     .join(prior_pre_class_shopper, 'household_id', 'leftanti')
+     .select('household_id', 'customer_macro_flag')
+     .withColumn('customer_micro_flag', F.lit('new_to_class'))
+    )
+
+    if switching_lv == 'subclass':
+        new_sku_new_subclass = \
+        (new_exposed_cust_and_sku_shopper
+         .join(prior_pre_store_shopper, 'household_id', 'inner')
+         .join(prior_pre_class_shopper, 'household_id', 'inner')
+         .join(prior_pre_subclass_shopper, 'household_id', 'leftanti')
+         .select('household_id', 'customer_macro_flag')
+         .withColumn('customer_micro_flag', F.lit('new_to_subclass'))
+        )
+
+        prior_pre_brand_in_subclass_shopper = \
+        (prior_pre_cc_txn
+         .join(sclass_df, "upc_id", "inner")
+         .join(brand_df, "upc_id")
+         .select('household_id')
+         .drop_duplicates()
+        )
+
+        #---- Current subclass shopper , new to brand : brand switcher within sublass
+        new_sku_new_brand_shopper = \
+        (new_exposed_cust_and_sku_shopper
+         .join(prior_pre_store_shopper, 'household_id', 'inner')
+         .join(prior_pre_class_shopper, 'household_id', 'inner')
+         .join(prior_pre_subclass_shopper, 'household_id', 'inner')
+         .join(prior_pre_brand_in_subclass_shopper, 'household_id', 'leftanti')
+         .select('household_id', 'customer_macro_flag')
+         .withColumn('customer_micro_flag', F.lit('new_to_brand'))
+        )
+
+        new_sku_within_brand_shopper = \
+        (new_exposed_cust_and_sku_shopper
+         .join(prior_pre_store_shopper, 'household_id', 'inner')
+         .join(prior_pre_class_shopper, 'household_id', 'inner')
+         .join(prior_pre_subclass_shopper, 'household_id', 'inner')
+         .join(prior_pre_brand_in_subclass_shopper, 'household_id', 'inner')
+         .join(prior_pre_sku_shopper, 'household_id', 'leftanti')
+         .select('household_id', 'customer_macro_flag')
+         .withColumn('customer_micro_flag', F.lit('new_to_sku'))
+        )
+
+        result_movement = \
+        (existing_exposed_cust_and_sku_shopper
+         .unionByName(new_sku_new_store)
+         .unionByName(new_sku_new_class)
+         .unionByName(new_sku_new_subclass)
+         .unionByName(new_sku_new_brand_shopper)
+         .unionByName(new_sku_within_brand_shopper)
+         .checkpoint()
+        )
+
+        return result_movement, new_exposed_cust_and_sku_shopper
+
+    elif switching_lv == 'class':
+
+        prior_pre_brand_in_class_shopper = \
+        (prior_pre_cc_txn
+         .join(class_df, "upc_id", "inner")
+         .join(brand_df, "upc_id")
+         .select('household_id')
+        ).drop_duplicates()
+
+        #---- Current subclass shopper , new to brand : brand switcher within class
+        new_sku_new_brand_shopper = \
+        (new_exposed_cust_and_sku_shopper
+         .join(prior_pre_store_shopper, 'household_id', 'inner')
+         .join(prior_pre_class_shopper, 'household_id', 'inner')
+         .join(prior_pre_brand_in_class_shopper, 'household_id', 'leftanti')
+         .select('household_id', 'customer_macro_flag')
+         .withColumn('customer_micro_flag', F.lit('new_to_brand'))
+        )
+
+        new_sku_within_brand_shopper = \
+        (new_exposed_cust_and_sku_shopper
+         .join(prior_pre_store_shopper, 'household_id', 'inner')
+         .join(prior_pre_class_shopper, 'household_id', 'inner')
+         .join(prior_pre_brand_in_class_shopper, 'household_id', 'inner')
+         .join(prior_pre_sku_shopper, 'household_id', 'leftanti')
+         .select('household_id', 'customer_macro_flag')
+         .withColumn('customer_micro_flag', F.lit('new_to_sku'))
+        )
+
+        result_movement = \
+        (existing_exposed_cust_and_sku_shopper
+         .unionByName(new_sku_new_store)
+         .unionByName(new_sku_new_class)
+         .unionByName(new_sku_new_brand_shopper)
+         .unionByName(new_sku_within_brand_shopper)
+         .checkpoint()
+        )
+
+        return result_movement, new_exposed_cust_and_sku_shopper
+
+    else:
+        print('Not recognized Movement and Switching level param')
+        return None, None
