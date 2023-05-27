42d4f35e8e97780db2eabb703d5cf13c8a263883
Danny Thanakrit <pb.danny@gmail.com>
2023-05-27 10:26:57 +0700

    update ctrl pre

--- cross_cate/asso_basket.py
+++ cross_cate/asso_basket.py
@@ -23,7 +23,8 @@ from functools import reduce
 def union_frame(left, right):
     return left.unionByName(right, allowMissingColumns=True)
 
-def get_txn_target_store_aisle_cross_cate(cmp: CampaignEval):
+#---- target dur
+def get_txn_target_store_aisle_cross_cate_dur(cmp: CampaignEval):
     """
     """
     exposed.create_txn_offline_x_aisle_target_store(cmp)
@@ -33,7 +34,7 @@ def get_txn_target_store_aisle_cross_cate(cmp: CampaignEval):
          )
     return txn_aisle_cross_cate
 
-def get_txn_target_store_feature(cmp: CampaignEval,
+def get_txn_target_store_feature_dur(cmp: CampaignEval,
                                   prd_scope_df: SparkDataFrame):
     """
     """
@@ -48,12 +49,12 @@ def get_txn_target_store_feature(cmp: CampaignEval,
         )
     return txn_target_store_feature
 
-def get_bask_asso(cmp: CampaignEval,
+def get_bask_asso_target_dur(cmp: CampaignEval,
                   prd_scope_df: SparkDataFrame):
     """
     """
-    aisle_txn = get_txn_target_store_aisle_cross_cate(cmp)
-    feat_txn = get_txn_target_store_feature(cmp, prd_scope_df)
+    aisle_txn = get_txn_target_store_aisle_cross_cate_dur(cmp)
+    feat_txn = get_txn_target_store_feature_dur(cmp, prd_scope_df)
 
     bask_asso = (aisle_txn
                  .select("transaction_uid").drop_duplicates()
@@ -64,12 +65,12 @@ def get_bask_asso(cmp: CampaignEval,
 
     return bask_asso
 
-def asso_score(cmp: CampaignEval,
+def asso_score_target_dur(cmp: CampaignEval,
                prd_scope_df: SparkDataFrame):
     
-    aisle_txn = get_txn_target_store_aisle_cross_cate(cmp)
-    feat_txn = get_txn_target_store_feature(cmp, prd_scope_df)
-    bask_asso = get_bask_asso(cmp, prd_scope_df)
+    aisle_txn = get_txn_target_store_aisle_cross_cate_dur(cmp)
+    feat_txn = get_txn_target_store_feature_dur(cmp, prd_scope_df)
+    bask_asso = get_bask_asso_target_dur(cmp, prd_scope_df)
 
     n_bask_aisle = aisle_txn.agg(F.count_distinct("transaction_uid")).collect()[0][0]
     n_bask_feat = feat_txn.agg(F.count_distinct("transaction_uid")).collect()[0][0]
@@ -83,13 +84,13 @@ def asso_score(cmp: CampaignEval,
 
     return score_df
 
-def asso_size(cmp: CampaignEval,
+def asso_size_target_dur(cmp: CampaignEval,
               prd_scope_df: SparkDataFrame):
     """
     """
-    aisle_txn = get_txn_target_store_aisle_cross_cate(cmp)
-    feat_txn = get_txn_target_store_feature(cmp, prd_scope_df)
-    bask_asso = get_bask_asso(cmp, prd_scope_df)
+    aisle_txn = get_txn_target_store_aisle_cross_cate_dur(cmp)
+    feat_txn = get_txn_target_store_feature_dur(cmp, prd_scope_df)
+    bask_asso = get_bask_asso_target_dur(cmp, prd_scope_df)
 
     size_feat_in_asso = \
         (feat_txn
@@ -99,8 +100,6 @@ def asso_size(cmp: CampaignEval,
               (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
          )
         .withColumn("bask_type", F.lit("feat_in_asso"))
-        .withColumn("period", F.lit("cmp"))
-        .withColumn("store_type", F.lit("test"))
         )
     size_feat = \
         (feat_txn
@@ -109,8 +108,7 @@ def asso_size(cmp: CampaignEval,
               (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
          )
          .withColumn("bask_type", F.lit("feat"))
-         .withColumn("period", F.lit("cmp"))
-         .withColumn("store_type", F.lit("test"))
+
         )
 
     size_aisle_in_asso = \
@@ -121,8 +119,7 @@ def asso_size(cmp: CampaignEval,
               (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
          )
          .withColumn("bask_type", F.lit("aisle_in_asso"))
-         .withColumn("period", F.lit("cmp"))
-         .withColumn("store_type", F.lit("test"))
+
         )
     size_aisle = \
         (aisle_txn
@@ -131,14 +128,17 @@ def asso_size(cmp: CampaignEval,
               (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
             )
          .withColumn("bask_type", F.lit("aisle"))
-         .withColumn("period", F.lit("cmp"))
-         .withColumn("store_type", F.lit("test"))
+
         )
 
-    combine_size = reduce(union_frame, [size_feat_in_asso, size_feat, size_aisle_in_asso, size_aisle])
+    combine = reduce(union_frame, [size_feat_in_asso, size_feat, size_aisle_in_asso, size_aisle])
+    combine_add_col = (combine.withColumn("period", F.lit("dur"))
+                       .withColumn("store_type", F.lit("test"))
+    )
 
-    return combine_size
+    return combine_add_col
 
+#---- target pre
 def get_txn_target_store_aisle_cross_cate_pre(cmp: CampaignEval):
     """
     """
@@ -172,7 +172,7 @@ def get_txn_target_store_feature_pre(cmp: CampaignEval,
         )
     return txn_target_store_feature_pre
 
-def get_bask_asso_pre(cmp: CampaignEval,
+def get_bask_target_asso_pre(cmp: CampaignEval,
                   prd_scope_df: SparkDataFrame):
     """
     """
@@ -188,12 +188,12 @@ def get_bask_asso_pre(cmp: CampaignEval,
 
     return bask_asso
 
-def asso_score_pre(cmp: CampaignEval,
+def asso_score_target_pre(cmp: CampaignEval,
                prd_scope_df: SparkDataFrame):
     
     aisle_txn = get_txn_target_store_aisle_cross_cate_pre(cmp)
     feat_txn = get_txn_target_store_feature_pre(cmp, prd_scope_df)
-    bask_asso = get_bask_asso_pre(cmp, prd_scope_df)
+    bask_asso = get_bask_target_asso_pre(cmp, prd_scope_df)
 
     n_bask_aisle = aisle_txn.agg(F.count_distinct("transaction_uid")).collect()[0][0]
     n_bask_feat = feat_txn.agg(F.count_distinct("transaction_uid")).collect()[0][0]
@@ -207,13 +207,13 @@ def asso_score_pre(cmp: CampaignEval,
 
     return score_df
 
-def asso_size_pre(cmp: CampaignEval,
+def asso_size_target_pre(cmp: CampaignEval,
                   prd_scope_df: SparkDataFrame):
     """
     """
     aisle_txn = get_txn_target_store_aisle_cross_cate_pre(cmp)
     feat_txn = get_txn_target_store_feature_pre(cmp, prd_scope_df)
-    bask_asso = get_bask_asso_pre(cmp, prd_scope_df)
+    bask_asso = get_bask_target_asso_pre(cmp, prd_scope_df)
 
     size_feat_in_asso = \
         (feat_txn
@@ -223,9 +223,6 @@ def asso_size_pre(cmp: CampaignEval,
               (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
          )
         .withColumn("bask_type", F.lit("feat_in_asso"))
-        .withColumn("period", F.lit("pre"))
-        .withColumn("store_type", F.lit("test"))
-
         )
     size_feat = \
         (feat_txn
@@ -234,9 +231,6 @@ def asso_size_pre(cmp: CampaignEval,
               (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
          )
          .withColumn("bask_type", F.lit("feat"))
-         .withColumn("period", F.lit("pre"))
-         .withColumn("store_type", F.lit("test"))
-
         )
 
     size_aisle_in_asso = \
@@ -247,9 +241,6 @@ def asso_size_pre(cmp: CampaignEval,
               (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
          )
          .withColumn("bask_type", F.lit("aisle_in_asso"))
-         .withColumn("period", F.lit("pre"))
-         .withColumn("store_type", F.lit("test"))
-
         )
     size_aisle = \
         (aisle_txn
@@ -258,16 +249,19 @@ def asso_size_pre(cmp: CampaignEval,
               (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
             )
          .withColumn("bask_type", F.lit("aisle"))
-         .withColumn("period", F.lit("pre"))
-         .withColumn("store_type", F.lit("test"))
+
 
         )
 
-    combine_size = reduce(union_frame, [size_feat_in_asso, size_feat, size_aisle_in_asso, size_aisle])
+    combine = reduce(union_frame, [size_feat_in_asso, size_feat, size_aisle_in_asso, size_aisle])
+    combine_add_col = (combine.withColumn("period", F.lit("pre"))
+                       .withColumn("store_type", F.lit("test"))
+    )
 
-    return combine_size
+    return combine_add_col
 
-def get_txn_ctrl_store_aisle_cross_cate(cmp: CampaignEval):
+#---- ctrl dur
+def get_txn_ctrl_store_aisle_cross_cate_dur(cmp: CampaignEval):
     """
     """
     store_matching.get_store_matching_across_region(cmp)
@@ -302,7 +296,7 @@ def get_txn_ctrl_store_aisle_cross_cate(cmp: CampaignEval):
     
     return txn_aisle_cross_cate_ctrl
 
-def get_txn_ctrl_store_feature(cmp: CampaignEval,
+def get_txn_ctrl_store_feature_dur(cmp: CampaignEval,
                                prd_scope_df: SparkDataFrame):
     """
     """
@@ -325,12 +319,12 @@ def get_txn_ctrl_store_feature(cmp: CampaignEval,
 
     return txn_ctrl_store_feature
 
-def get_bask_asso_ctrl(cmp: CampaignEval,
+def get_bask_asso_ctrl_dur(cmp: CampaignEval,
                        prd_scope_df: SparkDataFrame):
     """
     """
-    aisle_txn = get_txn_ctrl_store_aisle_cross_cate(cmp)
-    feat_txn = get_txn_ctrl_store_feature(cmp, prd_scope_df)
+    aisle_txn = get_txn_ctrl_store_aisle_cross_cate_dur(cmp)
+    feat_txn = get_txn_ctrl_store_feature_dur(cmp, prd_scope_df)
 
     bask_asso = (aisle_txn
                  .select("transaction_uid").drop_duplicates()
@@ -341,12 +335,12 @@ def get_bask_asso_ctrl(cmp: CampaignEval,
 
     return bask_asso
 
-def asso_score_ctrl(cmp: CampaignEval,
+def asso_score_ctrl_dur(cmp: CampaignEval,
                     prd_scope_df: SparkDataFrame):
     
-    aisle_txn = get_txn_ctrl_store_aisle_cross_cate(cmp)
-    feat_txn = get_txn_ctrl_store_feature(cmp, prd_scope_df)
-    bask_asso = get_bask_asso_ctrl(cmp, prd_scope_df)
+    aisle_txn = get_txn_ctrl_store_aisle_cross_cate_dur(cmp)
+    feat_txn = get_txn_ctrl_store_feature_dur(cmp, prd_scope_df)
+    bask_asso = get_bask_asso_ctrl_dur(cmp, prd_scope_df)
 
     n_bask_aisle = aisle_txn.agg(F.count_distinct("transaction_uid")).collect()[0][0]
     n_bask_feat = feat_txn.agg(F.count_distinct("transaction_uid")).collect()[0][0]
@@ -360,13 +354,13 @@ def asso_score_ctrl(cmp: CampaignEval,
 
     return score_df
 
-def asso_size_ctrl(cmp: CampaignEval,
+def asso_size_ctrl_dur(cmp: CampaignEval,
               prd_scope_df: SparkDataFrame):
     """
     """
-    aisle_txn = get_txn_ctrl_store_aisle_cross_cate(cmp)
-    feat_txn = get_txn_ctrl_store_feature(cmp, prd_scope_df)
-    bask_asso = get_bask_asso_ctrl(cmp, prd_scope_df)
+    aisle_txn = get_txn_ctrl_store_aisle_cross_cate_dur(cmp)
+    feat_txn = get_txn_ctrl_store_feature_dur(cmp, prd_scope_df)
+    bask_asso = get_bask_asso_ctrl_dur(cmp, prd_scope_df)
 
     size_feat_in_asso = \
         (feat_txn
@@ -376,8 +370,6 @@ def asso_size_ctrl(cmp: CampaignEval,
               (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
          )
         .withColumn("bask_type", F.lit("feat_in_asso"))
-        .withColumn("period", F.lit("cmp"))
-        .withColumn("store_type", F.lit("ctrl"))
         )
     size_feat = \
         (feat_txn
@@ -386,8 +378,6 @@ def asso_size_ctrl(cmp: CampaignEval,
               (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
          )
          .withColumn("bask_type", F.lit("feat"))
-         .withColumn("period", F.lit("cmp"))
-         .withColumn("store_type", F.lit("ctrl"))
         )
 
     size_aisle_in_asso = \
@@ -398,8 +388,6 @@ def asso_size_ctrl(cmp: CampaignEval,
               (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
          )
          .withColumn("bask_type", F.lit("aisle_in_asso"))
-         .withColumn("period", F.lit("cmp"))
-         .withColumn("store_type", F.lit("ctrl"))
         )
     size_aisle = \
         (aisle_txn
@@ -408,10 +396,156 @@ def asso_size_ctrl(cmp: CampaignEval,
               (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
             )
          .withColumn("bask_type", F.lit("aisle"))
-         .withColumn("period", F.lit("cmp"))
-         .withColumn("store_type", F.lit("ctrl"))
         )
 
-    combine_size = reduce(union_frame, [size_feat_in_asso, size_feat, size_aisle_in_asso, size_aisle])
+    combine = reduce(union_frame, [size_feat_in_asso, size_feat, size_aisle_in_asso, size_aisle])
+    combine_add_col = (combine.withColumn("period", F.lit("cmp"))
+                       .withColumn("store_type", F.lit("ctrl"))
+    )
 
-    return combine_size
+    return combine_add_col
+
+#---- ctrl pre
+def get_txn_ctrl_store_aisle_cross_cate_pre(cmp: CampaignEval):
+    """
+    """
+    store_matching.get_store_matching_across_region(cmp)
+    period_wk_col_nm = period_cal.get_period_wk_col_nm(cmp)
+    
+    cross_cate_target_store = \
+        (cmp.aisle_target_store_conf
+        .where(F.col("aisle_scope").isin(["cross_cate"]))
+        .select("store_id", "upc_id")
+        ).drop_duplicates()
+
+    matched_store_id = \
+        (cmp.matched_store
+        .select("test_store_id", "ctrl_store_id")
+        .drop_duplicates()
+        )
+
+    cross_cate_ctrl_store = \
+        (matched_store_id
+        .withColumnRenamed("test_store_id", "store_id")
+        .join(cross_cate_target_store, "store_id", "left")
+        .drop("store_id")
+        .withColumnRenamed("ctrl_store_id", "store_id")
+        )
+
+    txn_aisle_cross_cate_ctrl = \
+        (cmp.txn
+         .where(F.col("offline_online_other_channel")=="OFFLINE")
+         .where(F.col(period_wk_col_nm).isin(["pre"]))
+         .join(cross_cate_ctrl_store, ["store_id", "upc_id"])
+         )
+    
+    return txn_aisle_cross_cate_ctrl
+
+def get_txn_ctrl_store_feature_pre(cmp: CampaignEval,
+                               prd_scope_df: SparkDataFrame):
+    """
+    """
+    period_wk_col_nm = period_cal.get_period_wk_col_nm(cmp)
+
+    matched_ctrl_store_id = \
+        (cmp.matched_store
+        .select("ctrl_store_id")
+        .drop_duplicates()
+        .withColumnRenamed("ctrl_store_id", "store_id")
+        )
+
+    txn_ctrl_store_feature = \
+        (cmp.txn
+         .where(F.col("offline_online_other_channel")=="OFFLINE")
+         .join(matched_ctrl_store_id, "store_id")
+         .where(F.col(period_wk_col_nm).isin(["pre"]))
+         .join(prd_scope_df, 'upc_id')  
+        )
+
+    return txn_ctrl_store_feature
+
+def get_bask_asso_ctrl_pre(cmp: CampaignEval,
+                       prd_scope_df: SparkDataFrame):
+    """
+    """
+    aisle_txn = get_txn_ctrl_store_aisle_cross_cate_pre(cmp)
+    feat_txn = get_txn_ctrl_store_feature_pre(cmp, prd_scope_df)
+
+    bask_asso = (aisle_txn
+                 .select("transaction_uid").drop_duplicates()
+                 .join(feat_txn.select("transaction_uid").drop_duplicates(), 
+                       "transaction_uid", "inner")
+                 .select("transaction_uid")
+                 ).drop_duplicates()
+
+    return bask_asso
+
+def asso_score_ctrl_pre(cmp: CampaignEval,
+                    prd_scope_df: SparkDataFrame):
+    
+    aisle_txn = get_txn_ctrl_store_aisle_cross_cate_pre(cmp)
+    feat_txn = get_txn_ctrl_store_feature_pre(cmp, prd_scope_df)
+    bask_asso = get_bask_asso_ctrl_pre(cmp, prd_scope_df)
+
+    n_bask_aisle = aisle_txn.agg(F.count_distinct("transaction_uid")).collect()[0][0]
+    n_bask_feat = feat_txn.agg(F.count_distinct("transaction_uid")).collect()[0][0]
+    n_bask_asso = bask_asso.agg(F.count_distinct("transaction_uid")).collect()[0][0]
+
+    asso_by_aisle = n_bask_asso/n_bask_aisle
+    asso_by_feat = n_bask_asso/n_bask_feat
+
+    score_df = cmp.spark.createDataFrame([("ctrl","pre", n_bask_aisle, n_bask_feat, n_bask_asso, asso_by_aisle, asso_by_feat)],
+                                     ["store_type","period", "n_bask_aisle", "n_bask_feat", "n_bask_asso", "asso_by_aisle", "asso_by_feat"])
+
+    return score_df
+
+def asso_size_ctrl_pre(cmp: CampaignEval,
+                                     prd_scope_df: SparkDataFrame):
+    """
+    """
+    aisle_txn = get_txn_ctrl_store_aisle_cross_cate_pre(cmp)
+    feat_txn = get_txn_ctrl_store_feature_pre(cmp, prd_scope_df)
+    bask_asso = get_bask_asso_ctrl_pre(cmp, prd_scope_df)
+
+    size_feat_in_asso = \
+        (feat_txn
+         .join(bask_asso, "transaction_uid")
+         .agg(F.sum("net_spend_amt").alias("sales"),
+              F.count_distinct("transaction_uid").alias("visits"),
+              (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
+         )
+        .withColumn("bask_type", F.lit("feat_in_asso"))
+        )
+    size_feat = \
+        (feat_txn
+         .agg(F.sum("net_spend_amt").alias("sales"),
+              F.count_distinct("transaction_uid").alias("visits"),
+              (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
+         )
+         .withColumn("bask_type", F.lit("feat"))
+        )
+
+    size_aisle_in_asso = \
+        (aisle_txn
+         .join(bask_asso, "transaction_uid")
+         .agg(F.sum("net_spend_amt").alias("sales"),
+              F.count_distinct("transaction_uid").alias("visits"),
+              (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
+         )
+         .withColumn("bask_type", F.lit("aisle_in_asso"))
+        )
+    size_aisle = \
+        (aisle_txn
+         .agg(F.sum("net_spend_amt").alias("sales"),
+              F.count_distinct("transaction_uid").alias("visits"),
+              (F.sum("net_spend_amt")/F.count_distinct("transaction_uid")).alias("spv")
+            )
+         .withColumn("bask_type", F.lit("aisle"))
+        )
+
+    combine = reduce(union_frame, [size_feat_in_asso, size_feat, size_aisle_in_asso, size_aisle])
+    combine_add_col = (combine.withColumn("period", F.lit("pre"))
+                       .withColumn("store_type", F.lit("ctrl"))
+    )
+
+    return combine_add_col
