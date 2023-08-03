e6f82c9a026bf22aaaa597a9922e73b4b15cdcae
pbdanny <pb.danny@gmail.com>
2023-08-03 19:21:30 +0700

    add print of dropped table in cleanup

--- utils/cleanup.py
+++ utils/cleanup.py
@@ -26,10 +26,12 @@ def clear_attr_and_temp_tbl(cmp: CampaignEval):
     if hasattr(cmp, "aisle_target_store_conf"):
         delattr(cmp, "aisle_target_store_conf")
     tbl_nm = f"tdm_seg.th_lotuss_media_eval_aisle_target_store_conf_{cmp.params['cmp_id']}_temp"
+    print(f"Drop temp table (if exist) {tbl_nm}")
     spark.sql(f"DROP TABLE IF EXISTS {tbl_nm}")
     
     # clear cust_purchased_exposure_count
     tbl_nm_pattern = f"th_lotuss_media_eval_cust_purchased_exposure_count_{cmp.params['cmp_id'].lower()}_lv*"
     tables = spark.sql(f"SHOW TABLES IN tdm_seg LIKE '{tbl_nm_pattern}'")
     for row in tables.collect():
+        print(f"Drop temp table (if exist) tdm_seg.{row[1]}")
         spark.sql(f"DROP TABLE IF EXISTS tdm_seg.{row[1]}")
