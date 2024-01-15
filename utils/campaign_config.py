4ed4ef45ea2dafa69edba63439152e0db57671a2
pbdanny <pb.danny@gmail.com>
2024-01-15 15:19:09 +0700

    fix adjacency file

--- utils/campaign_config.py
+++ utils/campaign_config.py
@@ -1211,11 +1211,7 @@ class CampaignEvalO3(CampaignEvalTemplate):
         self.custom_ctrl_store_file = (
             self.cmp_inputs_files / f"control_store_{self.params['cmp_id']}.csv"
         )
-        # self.params["cross_cate_flag"] = ""
-        # self.params["cross_cate_cd"] = ""
-        
-        # self.cross_cate_flag = None
-        # self.cross_cate_cd = None
+        self.adjacency_file = self.std_input_path / f"{self.params['adjacency_file']}"
         
         self.load_period()
         self.load_target_store()
