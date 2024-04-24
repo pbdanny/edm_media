c576a756bfa56c4a08d32b7f7d789b49b2aed30c
Danny Thanakrit <pb.danny@gmail.com>
2024-04-24 20:16:30 +0700

    aisle target store path

--- utils/cleanup.py
+++ utils/cleanup.py
@@ -13,6 +13,7 @@ from pyspark.sql import functions as F
 from pyspark.sql import types as T
 from pyspark.sql import Window
 from pyspark.sql import DataFrame as SparkDataFrame
+from pyspark.dbutils import DBUtils
 
 from utils.DBPath import DBPath
 # from utils.campaign_config import CampaignEval, CampaignEvalO3
@@ -28,6 +29,8 @@ def clear_attr_and_temp_tbl(cmp):
     tbl_nm = f"tdm_dev.th_lotuss_media_eval_aisle_target_store_conf_{cmp.params['cmp_id']}_temp"
     print(f"Drop temp table (if exist) {tbl_nm}")
     spark.sql(f"DROP TABLE IF EXISTS {tbl_nm}")
+    
+    DBUtils.fs.rm(f"abfss://data-dev@pvtdmdlsazc02.dfs.core.windows.net/tdm_dev.db/th_lotuss_media_eval_aisle_target_store_conf_{cmp.params['cmp_id']}_temp", True)
     
     # clear cust_purchased_exposure_count
     tbl_nm_pattern = f"th_lotuss_media_eval_cust_purchased_exposure_count_{cmp.params['cmp_id'].lower()}_lv*"
